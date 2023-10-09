5ac963a6c67625eca3ffc1c18f9dd864b3b6c4da
Mips2648 <mips2648@gmail.com>
2023-10-09 13:34:00 +0200

    bump version

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '67823a73f4966d4563ab06267367edb07cab158e',
+    'name' => 'mips/jeedom-gotify',
+  ),
+  'versions' => 
+  array (
+    'mips/httpclient' => 
+    array (
+      'pretty_version' => 'v0.9',
+      'version' => '0.9.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2259a0e6bf4522f71e57ff5896a1171cce09cff9',
+    ),
+    'mips/jeedom-gotify' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '67823a73f4966d4563ab06267367edb07cab158e',
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.4',
+      'version' => '1.1.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+    ),
+  ),
+);
