915fd2c6de37b586b3ed09e7efb6d9a0f67113b2
Mips2648 <mips2648@gmail.com>
2024-10-10 15:32:40 +0200

    remove vendor from git

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -18,14 +18,23 @@
 
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
+function InstallComposerDependencies(string $pluginId) {
+    log::add($pluginId, 'info', 'Install composer dependencies');
+    $cmd = 'cd ' . __DIR__ . '/../;export COMPOSER_ALLOW_SUPERUSER=1;export COMPOSER_HOME="/tmp/composer";' . system::getCmdSudo() . 'composer install --no-ansi --no-dev --no-interaction --no-plugins --no-progress --no-scripts --optimize-autoloader;' . system::getCmdSudo() . ' chown -R www-data:www-data *';
+    shell_exec($cmd);
+}
+
 function gotify_install() {
     $pluginId = 'gotify';
+    InstallComposerDependencies($pluginId);
+
     config::save("api::{$pluginId}::mode", 'disable');
     config::save("api::{$pluginId}::restricted", 1);
 }
 
 function gotify_update() {
     $pluginId = 'gotify';
+    InstallComposerDependencies($pluginId);
 
     $clientToken = config::byKey('clientToken', $pluginId);
     /** @var gotify */
