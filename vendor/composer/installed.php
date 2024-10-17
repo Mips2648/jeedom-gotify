a2c9e394384e462dbf8070173073f8de9f9cc2f8
Mips2648 <mips2648@gmail.com>
2024-10-17 16:23:54 +0200

    restore vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3db21dd4e48809013d6606ec369012fb318e0f3a',
+    'name' => 'mips/jeedom-gotify',
+  ),
+  'versions' => 
+  array (
+    'mips/httpclient' => 
+    array (
+      'pretty_version' => 'v1.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fe44ea44d2411aba064c60cca041ee214e2fa737',
+    ),
+    'mips/jeedom-gotify' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3db21dd4e48809013d6606ec369012fb318e0f3a',
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.4',
+      'version' => '1.1.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+    ),
+  ),
+);
