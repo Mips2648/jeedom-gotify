8c4a0599ccfb02169f40801e31185c9564a77d84
Mips2648 <mips2648@gmail.com>
2024-11-01 10:58:56 +0100

    cleaning

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -31,7 +31,6 @@ function gotify_post_market_install() {
 
 function gotify_install() {
     $pluginId = basename(realpath(__DIR__ . '/..'));
-    InstallComposerDependencies();
 
     config::save("api::{$pluginId}::mode", 'disable');
     config::save("api::{$pluginId}::restricted", 1);
@@ -39,7 +38,6 @@ function gotify_install() {
 
 function gotify_update() {
     $pluginId = basename(realpath(__DIR__ . '/..'));
-    InstallComposerDependencies();
 
     $clientToken = config::byKey('clientToken', $pluginId);
     /** @var gotify */
